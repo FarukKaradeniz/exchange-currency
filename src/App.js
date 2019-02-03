f5c5fd537613286b02c80c08c08f0e7dcb6c539e
Ömer Faruk Karadeniz <krdnzomer@gmail.com>
2019-02-03 15:01:27 +0300

    Sketchy design is set for currency input

--- src/App.js
+++ src/App.js
@@ -8,13 +8,48 @@ import Footer from './components/Footer';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      firstCurrency: 1,
+      secondCurrency: 1,  
+    };
+
+  }
+
+  onCurrencyChanged = (e) => {
+    if (e.target.name === 'firstCurrency') {
+      const firstVal = e.target.value;
+      this.setState({ 
+        [e.target.name]: e.target.value,
+        secondCurrency: firstVal * 5.21,
+      }); //Sets the new value
+    }
+    else { //e.target.name === 'secondCurrency'
+      const secondVal = e.target.value;
+      this.setState({ 
+        [e.target.name]: e.target.value,
+        firstCurrency: secondVal * 0.19,
+      }); //Sets the new value
+    }
+
+  };
+
   render() {
     return (
       <main className="container">
         <Header />
         <div className="currencyview-wrapper">
-          <CurrencyView v={1}/>
-          <CurrencyView v={2}/>
+          <CurrencyView 
+            name={'firstCurrency'}
+            value={this.state.firstCurrency}
+            handleChange={this.onCurrencyChanged} 
+          />
+          <CurrencyView 
+            name={'secondCurrency'}
+            value={this.state.secondCurrency}
+            handleChange={this.onCurrencyChanged} 
+          />
           <div className="chartview-wrapper">
             <ChartView />
           </div>
--- src/components/CurrencyInputText.js
+++ src/components/CurrencyInputText.js
@@ -1,11 +1,20 @@
 import React from 'react';
 
+import '../styles/CurrencyInputText.css';
 
-export default class CurrencyInputText extends React.Component {
+export default class CurrencyInputText extends React.Component {  
   render() {
     return(
-      <div>
-        <h3>CurrencyInputText.h3</h3>
+      <div className="currency-input-text-wrapper">
+        <input 
+          type="number"
+          name={this.props.name}
+          value={this.props.value}
+          onChange={this.props.handleChange} 
+          className="currency-input-text" 
+          placeholder="Enter a currency value"
+          step="0.01"
+        />
       </div>
     );
   }
